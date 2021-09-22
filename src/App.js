0f9b9f3ab5f2f13c00448c1b9d3401c20494338e
Ivan Faryga <ifaryga@gmail.com>
2021-09-23 01:06:41 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Profile from "./components/socialProfile";
+import user from "./user.json";
+
+console.log(user);
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Profile
+        name={user.name}
+        avatar={user.avatar}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/socialProfile.jsx
+++ src/components/socialProfile.jsx
@@ -1,33 +1,44 @@
+import React from "react";
 import PropTypes from "prop-types";
 
-const Profile = ({ name, tag, location, avatar, followers, views, likes }) => {
+// {
+//   name, tag, location, avatar, followers, views, likes;
+// }
+
+const Profile = (props) => {
+  console.log(props);
   return (
-    <div class="profile">
-      <div class="description">
-        <img src={avatar} alt={name} class="avatar" />
-        <p class="name">{name}</p>
-        <p class="tag">{tag}</p>
-        <p class="location">{location}</p>
+    <div className="profile">
+      <div className="description">
+        <img
+          src={props.avpropsatar}
+          alt={props.name}
+          className="avatar"
+          width="480"
+        />
+        <p className="name">{props.name}</p>
+        <p className="tag">{props.tag}</p>
+        <p className="location">{props.location}</p>
       </div>
 
-      <ul class="stats">
+      <ul className="stats">
         <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{followers}</span>
+          <span className="label">Followers</span>
+          <span className="quantity">{props.stats.followers}</span>
         </li>
         <li>
-          <span class="label">Views</span>
-          <span class="quantity">{views}</span>
+          <span className="label">Views</span>
+          <span className="quantity">{props.stats.views}</span>
         </li>
         <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{likes}</span>
+          <span className="label">Likes</span>
+          <span className="quantity">{props.stats.likes}</span>
         </li>
       </ul>
     </div>
   );
 };
 
-Profile.PropTypes = {};
+Profile.propTypes = {};
 
 export default Profile;
